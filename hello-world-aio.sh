4dd3d084d3db5476b151862475b5cdd1b76dde65
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-26 15:22:13 -0400

    update all in one script

--- hello-world-aio.sh
+++ hello-world-aio.sh
@@ -5,5 +5,5 @@ docker run -td --rm -p 4300:80 --name oauth2-ui publicdevop2019/oauth-ui:hw
 docker run -td --rm -p 4200:80 --name object-market publicdevop2019/object-market:hw
 # end of ui #
 # start of micro-services #
-#docker run --rm -p 8111:8111 aio:latest $EMAIL_USERNAME $EMAIL_PWD
+docker run -td --rm -p 8111:8111 --name all-in-one publicdevop2019/all-in-one:latest $EMAIL_USERNAME $EMAIL_PWD
 # end of micro-services #
