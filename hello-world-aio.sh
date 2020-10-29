e6148215c162e2f6e76a220254cc8e452298aae7
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-10-29 13:53:15 -0400

    add mq script

--- hello-world-aio.sh
+++ hello-world-aio.sh
@@ -5,8 +5,8 @@ docker run -td --rm -p 4300:80 --name oauth2-ui publicdevop2019/oauth-ui:hw
 docker run -td --rm -p 4200:80 --name object-market publicdevop2019/object-market:hw
 # end of ui #
 # rabbitmq
-docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+docker run -td --rm --name rabbitmq -p 5672:5672 -p 15672:15672 --network="host" rabbitmq:3-management
 
 # start of micro-services #
-docker run -td --rm -p 8111:8111 --name all-in-one publicdevop2019/all-in-one:latest $EMAIL_USERNAME $EMAIL_PWD
+docker run -td --rm -p 8111:8111 --name all-in-one  --network="host" publicdevop2019/all-in-one:latest $EMAIL_USERNAME $EMAIL_PWD
 # end of micro-services #
