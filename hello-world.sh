47ebd4bc33f91695eb6b95955fd9b5692d19ac37
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-11-29 19:10:24 -0500

    update helloword script

--- hello-world.sh
+++ hello-world.sh
@@ -1,24 +1,42 @@
 REGISTRY=--service.url.eureka=http://localhost:8761/eureka
+REGISTRY_IP=--eureka.instance.ip-address=127.0.0.1
 OAUTH=--service.url.oauth2=http://localhost:8080
+DATA_SOURCE_URL=--aws-instance-uri=localhost
+DATA_SOURCE_DDL=--spring.jpa.hibernate.ddl-auto=create
+DATA_SOURCE_USERNAME=--spring.datasource.username=root
 EMAIL_USERNAME=--spring.mail.username={replace_w_gmail_account}
 EMAIL_PWD=--spring.mail.password={replace_w_gmail_pwd}
+DEBUG_ENABLE=logging.level.com.hw=DEBUG
+# mongodb
+docker run -td --rm --name mongodb --network="host" -v "/$(pwd)/configs:/etc/mongo" -v ~/db/mongo:/data/db mongo:4.4.2-bionic --config /etc/mongo/mongod.conf
+sleep 3
+sh scripts/mongo-replica-set.sh
+# mysql
+docker run -td --rm --name mysqldb --network="host" -v ~/db/mysql:/var/lib/mysql -v ~/logs:/logs -e MYSQL_ALLOW_EMPTY_PASSWORD=1 mysql:8.0.22
+sleep 30
+sh scripts/mysql-db.sh
+# redis cache
+docker run -td --rm -v ~/db/cache:/data --name redis-cache --network="host" redis:6.0.8
 # rabbitmq
-docker run -td --rm --name rabbitmq --network="host" -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+docker run -td --rm --name rabbitmq --network="host" rabbitmq:3-management
 # start of ui #
 docker run -td --rm -p 4300:80 --name oauth2-ui publicdevop2019/oauth-ui:hw
 docker run -td --rm -p 4200:80 --name object-market publicdevop2019/object-market:hw
 # end of ui #
+
 # start of micro-services #
 docker run -td --rm --name eureka --network="host" publicdevop2019/eureka:latest -jar Eureka.jar
-docker run -td --rm --name oauth2 --network="host" publicdevop2019/oauth2service:hw -jar AuthService.jar $REGISTRY
+docker run -td --rm --name validator --network="host" publicdevop2019/validator:latest
+docker run -td --rm --name oauth2 --network="host" publicdevop2019/oauth2service:latest -jar AuthService.jar $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME $DEBUG_ENABLE
 sleep 20
-docker run -td --rm --name proxy --network="host" publicdevop2019/edgeproxy:hw -jar EdgeProxyService.jar $OAUTH $REGISTRY
+docker run -td --rm --name proxy --network="host" publicdevop2019/edgeproxy:latest -jar EdgeProxyService.jar $OAUTH $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME
+docker run -td --rm --name objectstore --network="host" publicdevop2019/object-store:latest -jar ObjectStore.jar $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL
 
-docker run -td --rm --name messenger --network="host" publicdevop2019/messenger:hw -jar Messenger.jar $EMAIL_USERNAME $EMAIL_PWD $REGISTRY
-docker run -td --rm --name file-upload --network="host" publicdevop2019/file-upload:hw -jar FileUpload.jar $REGISTRY
-docker run -td --rm --name payment --network="host" publicdevop2019/payment:hw -jar Payment.jar $REGISTRY
-docker run -td --rm --name saga-orchestrator --network="host" publicdevop2019/saga-orchestrator:hw -jar SagaOrchestrator.jar $REGISTRY
+docker run -td --rm --name messenger --network="host" publicdevop2019/messenger:hw -jar Messenger.jar $EMAIL_USERNAME $EMAIL_PWD $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME
+docker run -td --rm --name file-upload --network="host" publicdevop2019/file-upload:hw -jar FileUpload.jar $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME
+docker run -td --rm --name payment --network="host" publicdevop2019/payment:hw -jar Payment.jar $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME
+docker run -td --rm --name saga-orchestrator --network="host" publicdevop2019/saga-orchestrator:hw -jar SagaOrchestrator.jar $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME
 
-docker run -td --rm --name profile --network="host" publicdevop2019/userprofile:hw -jar UserProfile.jar $REGISTRY
-docker run -td --rm --name product --network="host" publicdevop2019/product:hw -jar Product.jar $REGISTRY
+docker run -td --rm --name profile --network="host" publicdevop2019/userprofile:hw -jar UserProfile.jar $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME
+docker run -td --rm --name product --network="host" publicdevop2019/product:hw -jar Product.jar $REGISTRY $REGISTRY_IP $DATA_SOURCE_URL $DATA_SOURCE_DDL $DATA_SOURCE_USERNAME
 # end of micro-services #
